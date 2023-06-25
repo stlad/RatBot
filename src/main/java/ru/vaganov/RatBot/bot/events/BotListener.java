7e9a0723c2b73ed8a6fa1fab07988b3a98cc556c
stlad <82213506+stlad@users.noreply.github.com>
2023-06-25 19:30:12 +0500

    init

---
+++ src/main/java/ru/vaganov/RatBot/bot/events/BotListener.java
@@ -0,0 +1,30 @@
+package ru.vaganov.RatBot.bot.events;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class BotListener extends ListenerAdapter {
+
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event){
+        if(event.getGuild()==null)
+            return;
+
+        switch (event.getName()){
+            case "say":
+                say(event, event.getOption("content").getAsString());
+
+            default:
+                event.reply("I can't handle that command right now :(").setEphemeral(true).queue();
+        }
+    }
+
+
+    private void say(SlashCommandInteractionEvent event, String content){
+        event.reply(content).queue();
+    }
+
+
+
+}
